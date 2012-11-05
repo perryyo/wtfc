eefa4baf966d92c56068dea301a9ac57618f27ce
Randall Degges <rdegges@gmail.com>
2012-11-04 18:56:19 -0800

    fuck

--- whothefuckcalled/settings/prod.py
+++ whothefuckcalled/settings/prod.py
@@ -5,7 +5,7 @@ from os import environ
 
 from memcacheify import memcacheify
 from postgresify import postgresify
-from S3 import CallingFormat
+#from S3 import CallingFormat
 
 from common import *
 
@@ -85,7 +85,7 @@ INSTALLED_APPS += (
 STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
+#AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 
 # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
 AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
