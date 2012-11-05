08d01f9e003f7569e2d015d88841f0cbc39b25c9
Randall Degges <rdegges@gmail.com>
2012-11-04 20:08:09 -0800

    Removing django app.

--- whothefuckcalled/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from requests import get
-
-
-def index(request):
-    number = request.REQUEST.get('number')
-    name = get('https://api.opencnam.com/v2/phone/%s' % number).text if number else ''
-
-    return render(request, 'index.html', {'name': name})
